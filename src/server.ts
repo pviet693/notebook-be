d2148b619ab4eef915d025f1f0daf8e44748c592
Phạm Văn Việt <47064221+pviet693@users.noreply.github.com>
2024-12-24 23:48:32 +0700

    feat: increase payload size limit

--- src/server.ts
+++ src/server.ts
@@ -27,8 +27,8 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "5mb" }));
 
 // socket io instance
 app.set("io", io);
