f516181f22d0d9608fb90e1e2402149bbebb1649
Phạm Văn Việt <47064221+pviet693@users.noreply.github.com>
2024-12-29 10:28:05 +0700

    feat: return cloudfront url after upload file

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 
 import { s3 } from "@/configs/s3.config";
 import { AppError } from "@/types/AppError";
+import { genCloudFrontFileUrl } from "@/utils";
 
 class UploadService {
     public static async upload(file: Express.Multer.File) {
@@ -25,10 +26,13 @@ class UploadService {
         };
 
         // Upload to S3
-        const data = await s3.upload(params).promise();
+        await s3.upload(params).promise();
+
+        // Cloudfront url of file
+        const cloudfrontUrl = genCloudFrontFileUrl(fileName);
 
         return {
-            fileUrl: data.Location
+            fileUrl: cloudfrontUrl
         };
     }
 }
